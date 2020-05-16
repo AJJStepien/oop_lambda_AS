05fbbebaeb402ccbf74f82979c9b686e268d036b
Adam Stępień <ajjstepien@gmail.com>
2020-05-16 10:37:01 +0200

    +getter, setter

--- src/main/java/sda/oop/Main.java
+++ src/main/java/sda/oop/Main.java
@@ -1,8 +1,13 @@
 package sda.oop;
 
+import sda.oop.model.User;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        User u1 = new User("Adam", "Stępień", "a@a.com","1234");
+        System.out.println(u1.toString());
     }
 }
--- src/main/java/sda/oop/model/User.java
+++ src/main/java/sda/oop/model/User.java
@@ -8,8 +8,83 @@ public class User {
     private String name, lastName, email, passwd;
 
     private LocalDateTime registrationDate = LocalDateTime.now();
-    private boolean isActive = true;
+    private boolean status = true;
 
     private Role role = Role.ROLE_USER;
 
+    public User(String name, String lastName, String email, String passwd) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.passwd = passwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", passwd='" + passwd + '\'' +
+                ", registrationDate=" + registrationDate +
+                ", status=" + status +
+                ", role=" + role +
+                '}';
+    }
 }
